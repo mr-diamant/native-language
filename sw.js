b29ae1221682da47b98c54d29e0dad6bb910749c
Vitaliy Chernigin <d9899842@gmail.com>
2026-09-13 16:46:28 +0500

    bump sw cache version to v15 and redeploy

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'native-language-v14';
+const CACHE_NAME = 'native-language-v15';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=14',
-  './app.js?v=14',
+  './styles.css?v=15',
+  './app.js?v=15',
   './manifest.json',
   './icons.svg',
   './api.php',
